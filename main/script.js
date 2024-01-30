975e537cb51264ed58d6ec9c96d7db44365728b1
Catboy522onroblox <97002444+Catboy522onroblox@users.noreply.github.com>
2024-01-29 23:30:22 -0500

    Delete main/script.js

--- main/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const contentSections = document.querySelectorAll('.content');
-
-    contentSections.forEach(section => {
-        section.style.opacity = '1';
-        section.style.transform = 'translateY(0)';
-    });
-});
