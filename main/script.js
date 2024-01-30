57e19f854a1b29529e820632a5df0c1501657f61
Catboy522onroblox <97002444+Catboy522onroblox@users.noreply.github.com>
2024-01-29 22:56:56 -0500

    Create script.js

---
+++ main/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll('.content').forEach(function(content) {
+        content.style.opacity = '1';
+        content.style.transform = 'translateY(0)';
+    });
+});
